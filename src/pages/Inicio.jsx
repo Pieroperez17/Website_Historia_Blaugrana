5bf1159f347b909fde3ebc08718590c879230ad8
Pieroperez17 <sppgcorreoinfo@gmail.com>
2025-12-07 00:37:16 -0500

    Mejoras

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,6 +4,9 @@ import React from "react";
 import { CategoryCarousel } from "../components/CategoryCarousel.jsx";
 import { ThreeDot } from 'react-loading-indicators'
 import {FooterInfo} from "../components/FooterInfo.jsx";
+import {ViewCardProduct} from "../components/ViewCardProduct.jsx";
+
+
 import './EstilosGenerales.css';
 
 
@@ -49,6 +52,50 @@ export default function Inicio() {
     return;
     }, []);
 
+    const [NuevosIngresosData, setNuevosIngresosData] = useState([]);
+        useEffect(() => {
+            const baseUrl = import.meta.env.VITE_API_Url ;
+            const apiAuth = import.meta.env.VITE_API_Authorization ;
+    
+            if (!baseUrl) {
+                console.error("API base URL not found in environment variables.");
+                console.log("Current env vars:", baseUrl);
+                return;
+            }
+            if (!apiAuth) {
+                console.error("API authorization token not found in environment variables.");
+                return;
+            }
+    
+    
+        async function fetchCategories() {
+            try {
+                const res = await fetch(`${baseUrl}/api/products?populate=*`, {
+                    method: "GET",
+                    headers: {
+                        ...(apiAuth ? { Authorization: apiAuth } : {}),
+                    },
+                });
+                if (!res.ok) throw new Error(`Fetch error: ${res.status}`);
+                const json = await res.json();
+                // If the API returns an array use it directly, otherwise try common wrappers
+                setNuevosIngresosData(json.data);
+                setIsLoading(false);
+                console.log(json.data);
+                //console.log(json.data[0].imagenRef.url);
+            } catch (err) {
+                if (err.name !== "AbortError") console.error("Error loading categories:", err);
+            }
+        }
+    
+        fetchCategories();
+        return;
+        }, []);
+    
+    const Nuevos_Ingresos = NuevosIngresosData.filter(producto => producto.categories.some(cat => cat.nombre === "Nuevo Ingreso") )
+
+
+
     return (
         <Layout>
             <main style={styles.container} >
@@ -58,8 +105,28 @@ export default function Inicio() {
                         <ThreeDot variant="bounce" color="#4492C2" size="small" text="" textColor="" />
                     </div>
                 ) : (
-                    <CategoryCarousel categories={clubesData.filter( club => club.tipo == 'Club')} />
+                    <>
+                        <h1 style={{margin:'0px'}}>Clubes</h1>
+                        <CategoryCarousel categories={clubesData.filter( club => club.tipo == 'Club')} />
+                    </>
                 )}
+                { isLoading ? (
+                    <div style={{ display: 'flex', gap:'2rem'  ,justifyContent: 'center', alignItems: 'center', height: '20vh' }}>
+                        <ThreeDot variant="bounce" color="#4492C2" size="small" text="" textColor="" />
+                    </div>
+                ) : (
+                    <>
+                        <h1>Nuevos Ingresos</h1>
+                        <div className="container-club">
+                            {Nuevos_Ingresos.map((producto) => (
+                                <ViewCardProduct key={producto.id} product={producto} />
+                            ))}
+                        </div>
+                    </>
+                    
+                )}
+
+
                 <FooterInfo />
             </main>
         </Layout>
