9664e92d8afd70e3c59f565af179dec02fda978b
Pieroperez17 <sppgcorreoinfo@gmail.com>
2026-01-27 23:24:05 -0500

    mejoras y aumento de data

--- src/data/Products.js
+++ src/data/Products.js
@@ -85,14 +85,218 @@ const products = [
             },
         ],
         categories: [
-            {
-                id: 1,
-                nombre: "Barcelona",
-            },
-            {
-                id: 2,
-                nombre: "Nuevo Ingreso",
-            }
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" },
+        ]
+    },
+    {
+        id: 2,
+        documentId: "a1b2c3d4e5f6g7h8i9j0",
+        nombre: "CAMISETA VALENCIA LOCAL PUMA ORIGINAL VERSION LA LIGA TEMPORADA 2019",
+        descripcion: "🔗 CAMISETA VALENCIA LOCAL PUMA ORIGINAL VERSION LA LIGA TEMPORADA 2019\n\nTALLA : M ADULTOS\n\nMEDIDAS AL DM 📥\n\n💲PRECIO : 120 SOLES\n✔️ESTADO : 9/10 PARCHE OFICIAL TRAÍDA DE ESPAÑA\n♻️ SEPARA CON s/10 SOLES♻️\n🚚ENVÍOS A TODO EL PERÚ",
+        stock: "1",
+        talla: "M",
+        precio: 120,
+        imagen: [
+            { id: 1, name: "img1", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769568107/producto2-1_vyu5jv.jpg" },
+            { id: 2, name: "img2", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769568108/producto2-2_dvfmn9.jpg" },
+            { id: 3, name: "img3", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769568111/producto2-3_itoae7.jpg" },
+            { id: 4, name: "img4", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769568113/producto2-4_tmz1vk.jpg" },
+            { id: 5, name: "img5", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769568115/producto2-5_mmcexa.jpg" },
+            { id: 6, name: "img6", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769568117/producto2-6_tqkopz.jpg" },
+            { id: 7, name: "img7", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769568120/producto2-7_rbbgqh.jpg" }
+        ],
+        categories: [
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" }
+        ]
+    },
+    {
+        id: 3,
+        documentId: "z9y8x7w6v5u4t3s2r1q0",
+        nombre: "CAMISETA REAL MADRID LOCAL ADIDAS ORIGINAL TEMPORADA 13/14",
+        descripcion: "🔗 CAMISETA REAL MADRID LOCAL ADIDAS ORIGINAL TEMPORADA 13/14\n\nTALLA : M ADULTOS\n\nMEDIDAS AL DM 📥\n\n💲PRECIO : 160 SOLES NEGOCIABLE\n✔️ESTADO : 9/10 DORSAL OFICIAL\n♻️ SEPARA CON s/10 SOLES♻️\n🚚ENVÍOS A TODO EL PERÚ",
+        stock: "1",
+        talla: "M",
+        precio: 160,
+        imagen: [
+            { id: 1, name: "img1", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569404/producto3-1_ync7y3.jpg" },
+            { id: 2, name: "img2", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569508/producto3_-2_xrqxvo.jpg" },
+            { id: 3, name: "img3", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569404/producto3_1_ytgljm.jpg" },
+            { id: 4, name: "img4", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569405/producto3_8_omhi6k.jpg" },
+            { id: 5, name: "img5", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569404/producto3_3_v2outh.jpg" },
+            { id: 6, name: "img6", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569405/producto3_10_qfsi7u.jpg" },
+            { id: 7, name: "img7", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569405/producto3_6_fxcvay.jpg" },
+            { id: 8, name: "img8", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569405/producto3_5_ukd8cc.jpg" },
+            { id: 9, name: "img9", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569405/producto3_11_hxd3yx.jpg" },
+            { id: 10, name: "img10", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569420/producto3_12_fwdl58.jpg" },
+            { id: 11, name: "img11", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569508/producto3_13_cjxp1b.jpg" },
+            { id: 12, name: "img12", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769569508/producto3_14_mdww7v.jpg" }
+        ],
+        categories: [
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" }
+        ]
+    },
+    {
+        id: 4,
+        documentId: "m4n5b6v7c8x9z0a1s2d3",
+        nombre: "CAMISETA FC BARCELONA  NIKE ORIGINAL TRAINING 2022 / 2023",
+        descripcion: "🔗 CAMISETA FC BARCELONA  NIKE ORIGINAL TRAINING 2022 / 2023\n\nTALLA : S ADULTOS\n\n MEDIDAS AL DM 📥\n\n💲PRECIO : 100 SOLES\n✔️ESTADO : 9.5/10\n♻️ SEPARA CON s/10 SOLES♻️\n🚚ENVÍOS A TODO EL PERÚ",
+        stock: "1",
+        talla: "S",
+        precio: 100,
+        imagen: [
+            { id: 1, name: "img1", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570051/producto4_1_rcbfxu.jpg" },
+            { id: 2, name: "img2", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570050/producto4_5_qnoxiu.jpg" },
+            { id: 3, name: "img3", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570050/producto4_6_cd0ek7.jpg" },
+            { id: 4, name: "img4", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570050/producto4_6_cd0ek7.jpg" },
+            { id: 5, name: "img5", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570050/producto4_6_cd0ek7.jpg" },
+            { id: 6, name: "img6", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570051/producto4_2_mzovjc.jpg" },
+            { id: 7, name: "img7", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570052/producto4_3_mwccwc.jpg" },
+            { id: 8, name: "img8", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769570053/producto4_4_tjdo5x.jpg" },
+        ],
+        categories: [
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" }
+        ]
+    },
+    {
+        id: 5,
+        documentId: "q1w2e3r4t5y6u7i8o9p0",
+        nombre: "CAMISETA FC BARCELONA LOCAL NIKE ORIGINAL TEMPORADA 2009 / 2010",
+        descripcion: "🔗 CAMISETA FC BARCELONA LOCAL NIKE ORIGINAL TEMPORADA 2009 / 2010\n\nTALLA : XL ADULTOS\n\nMEDIDAS AL DM 📥\n\n💲PRECIO : 290 SOLES\n✔️ESTADO : 9/10\n♻️ SEPARA CON s/10 SOLES♻️\n🚚ENVÍOS A TODO EL PERÚ",
+        stock: "1",
+        talla: "XL",
+        precio: 290,
+        imagen: [
+            { id: 1, name: "img1", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571396/producto5_1_x0o51l.jpg" },
+            { id: 2, name: "img2", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571400/producto5_8_bzmh5f.jpg" },
+            { id: 3, name: "img3", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571397/producto5_2_uqrfrf.jpg" },
+            { id: 5, name: "img5", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571397/producto5_3_hkz4ef.jpg" },
+            { id: 6, name: "img6", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571397/producto5_5_vexb9r.jpg" },
+            { id: 7, name: "img7", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571398/producto5_4_kj6fxb.jpg" },
+            { id: 8, name: "img8", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571399/producto5_7_rw5h9f.jpg" },
+            { id: 9, name: "img9", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571401/producto5_9_yybl6l.jpg" }
+        ],
+        categories: [
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" }
+        ]
+    },
+    {
+        id: 6,
+        documentId: "l0k9j8h7g6f5d4s3a2z1",
+        nombre: "CAMISETA FC BARCELONA LOCAL NIKE ORIGINAL TEMPORADA 2016",
+        descripcion: "🔗 CAMISETA FC BARCELONA LOCAL NIKE ORIGINAL TEMPORADA 2016\n\nTALLA : S ADULTOS FIT M\n\nMEDIDAS AL DM 📥\n\n💲PRECIO : 250 SOLES\n✔️ESTADO : 9/10\n♻️ SEPARA CON s/10 SOLES♻️\n🚚ENVÍOS A TODO EL PERÚ",
+        stock: "1",
+        talla: "S",
+        precio: 250,
+        imagen: [
+            { id: 1, name: "img1", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571955/producto6_1_udsv97.jpg" },
+            { id: 2, name: "img2", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571954/producto6_12_n6vmh1.jpg" },
+            { id: 3, name: "img3", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571955/producto6_2_ubc2nf.jpg" },
+            { id: 4, name: "img4", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571956/producto6_3_mutacm.jpg" },
+            { id: 5, name: "img5", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571957/producto6_4_vvxo6o.jpg" },
+            { id: 6, name: "img6", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571958/producto6_5_hvh2fu.jpg" },
+            { id: 7, name: "img7", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571959/producto6_6_thi2z5.jpg" },
+            { id: 8, name: "img8", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571960/producto6_7_qlpvii.jpg" },
+            { id: 9, name: "img9", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571961/producto6_8_ofg9re.jpg" },
+            { id: 10, name: "img10", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571962/producto6_9_zouwdj.jpg" },
+            { id: 11, name: "img11", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571963/producto6_10_paectr.jpg" },
+            { id: 12, name: "img12", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769571964/producto6_11_hlad5i.jpg" }
+        ],
+        categories: [
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" }
+        ]
+    },
+    {
+        id: 7,
+        documentId: "p0o9i8u7y6t5r4e3w2q1",
+        nombre: "CAMISETA FC BARCELONA SENYERA NIKE ORIGINAL TEMPORADA 2015 / 2016",
+        descripcion: "🔗 CAMISETA FC BARCELONA SENYERA NIKE ORIGINAL TEMPORADA 2015 / 2016\n\nTALLA : S ADULTOS FIT M\n\nMEDIDAS AL DM 📥\n\n💲PRECIO : 150 SOLES\n✔️ESTADO : 10/10\n♻️ SEPARA CON s/10 SOLES♻️\n🚚ENVÍOS A TODO EL PERÚ",
+        stock: "1",
+        talla: "S",
+        precio: 150,
+        imagen: [
+            { id: 1, name: "img1", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572493/producto7_11_ulnzz5.jpg" },
+            { id: 2, name: "img2", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572489/producto7_8_dgojas.jpg" },
+            { id: 3, name: "img3", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572491/producto7_10_yuv2ag.jpg" },
+            { id: 4, name: "img4", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572490/producto7_9_fgmzn1.jpg" },
+            { id: 5, name: "img5", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572521/producto7_12_mqmrvi.jpg" },
+            { id: 6, name: "img6", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572522/producto7_13_i0oeri.jpg" },
+            { id: 7, name: "img7", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572524/producto7_1_xa1kwh.jpg" },
+            { id: 8, name: "img8", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572525/producto7_2_tbcclo.jpg" },
+            { id: 9, name: "img9", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572527/producto7_3_dbruow.jpg" },
+            { id: 10, name: "img10", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572529/producto7_4_efjp76.jpg" },
+            { id: 11, name: "img11", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572531/producto7_5_nmdcgz.jpg" },
+            { id: 12, name: "img12", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572533/producto7_6_vsr2ei.jpg" }, 
+            { id: 13, name: "img13", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572534/producto7_7_na12wc.jpg" }
+        ],
+        categories: [
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" }
+        ]
+    },
+    {
+        id: 8,
+        documentId: "s3d4f5g6h7j8k9l0p1o2",
+        nombre: "CAMISETA FC BARCELONA TRAINING 2015 / 2016",
+        descripcion: "🔗 CAMISETA FC BARCELONA TRAINING 2015 / 2016\n\nTALLA : XL ADULTOS\n\nMEDIDAS AL DM 📥\n\n💲PRECIO : 100 SOLES NEGOCIABLE\n✔️ESTADO : 8.5/10 revisar fotos\n♻️ SEPARA CON s/10 SOLES♻️\n🚚ENVÍOS A TODO EL PERÚ",
+        stock: "1",
+        talla: "XL",
+        precio: 100,
+        imagen: [
+            { id: 1, name: "img1", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572543/producto8_7_zqoja3.jpg" },
+            { id: 2, name: "img2", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572537/producto8_4_f1rbns.jpg" },
+            { id: 3, name: "img3", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572540/producto8_6_ml9abe.jpg" },
+            { id: 4, name: "img4", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572539/producto8_5_x7ies8.jpg" },
+            { id: 5, name: "img5", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572546/producto8_9_eoyyws.jpg" },
+            { id: 6, name: "img6", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572551/producto8_12_vqj0xt.jpg" },
+            { id: 7, name: "img7", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572552/producto8_13_kgbduu.jpg" },
+            { id: 8, name: "img8", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572561/producto8_2_vmckv1.jpg" },
+            { id: 9, name: "img9", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572563/producto8_3_fsaqqx.jpg" },
+            { id: 10, name: "img10", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572554/producto8_14_dnypsb.jpg" },
+            { id: 11, name: "img11", url: "https://res.cloudinary.com/ds4u9u1zv/image/upload/v1769572559/producto8_1_yyrkak.jpg" }
+        ],
+        categories: [
+            { id: 1, nombre: "UEFA" },
+            { id: 2, nombre: "LaLiga" },
+            { id: 3, nombre: "Nuevo Ingreso" }
+        ]
+    },
+    {
+        id: 9,
+        documentId: "x1c2v3b4n5m6z7a8s9d0",
+        nombre: "BUFANDA RETRO 1992",
+        descripcion: "Bufanda retro del club año 1992. Pequeñas señales de uso, ideal para coleccionistas.",
+        stock: "10",
+        talla: "",
+        precio: 25,
+        imagen: [
+            { id: 1, name: "img1", url: "" },
+            { id: 2, name: "img2", url: "" },
+            { id: 3, name: "img3", url: "" },
+            { id: 4, name: "img4", url: "" },
+            { id: 5, name: "img5", url: "" },
+            { id: 6, name: "img6", url: "" },
+            { id: 7, name: "img7", url: "" },
+            { id: 8, name: "img8", url: "" },
+            { id: 9, name: "img9", url: "" },
+            { id: 10, name: "img10", url: "" },
+            { id: 11, name: "img11", url: "" }
+        ],
+        categories: [
+            { id: 4, nombre: "Memorabilia" },
+            { id: 10, nombre: "Accesorios" }
         ]
     }
 ];
--- src/pages/Clubes.jsx
+++ src/pages/Clubes.jsx
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 import  categorias  from "../data/Categoria.js";
 
 export default function Clubes() {
-    
-
     return (
         <Layout>
             <>
